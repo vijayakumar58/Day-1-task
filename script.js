5984f3443136eb92f32534f915673e0ac6433052
vijayakumar58 <vijayalingam58@gmail.com>
2022-10-09 12:18:44 +0530

    add js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// input output question & answer
+// question - 1
 // Getting input via STDIN
 const readline = require("readline");
 
@@ -17,3 +19,48 @@ inp.on("close", () => {
        var a = parseInt(userInput[0]);
        console.log(a);
     });
+
+    // question - 2
+    // Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  
+   var line1 = userInput[0];
+   console.log(line1);
+});
+
+// question - 3
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  
+   var line1 = userInput[0];
+   var line2 = userInput[1];
+    console.log(line1);
+  console.log(line2);
+
+});
