2104ba4e8b210badfeef83e4e479c4899f3bf23c
vijayakumar58 <vijayalingam58@gmail.com>
2022-10-09 12:20:24 +0530

    add file

--- script.js
+++ script.js
@@ -64,3 +64,27 @@ inp.on("close", () => {
   console.log(line2);
 
 });
+// question - 4
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  
+   var line1 = userInput[0];
+   var line2 = userInput[1];
+   var line3 = userInput[2];
+   console.log(line1);
+  console.log(line2);
+  console.log(line3);
+});
